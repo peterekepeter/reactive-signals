2da43d88aa1167afbbfb4efcdb28e008b6072305
Eke Péter <peterekepeter@gmail.com>
2020-01-18 02:47:25 +0200

    Tests, improvements, typedefs

--- lib/signal.d.ts
+++ lib/signal.d.ts
@@ -14,10 +14,49 @@
  *      currentUser = { id: 13, admin: true };
  *      
  */
-export function createSignal<T>(initialValue : T, options : ISignalOptions) : ISignal<T>;
+export function createSignal<T>(initialValue : T, options : ISignalOptions<T>) : ISignal<T>;
 
 
-export interface ISignalOptions {
+export interface ISignalOptions<T> {
+
+    /** is used to compare previous and next values, 
+     * if equal then event is not dispatched */
+    equals : (a : T, b : T) => boolean,
+
+    /** allows transformation of signal value, input or inputs */
+    transform: (value: any | any[]) => any,
+
+    /** use another signal as input for this signal,
+     * useful when combined with transform
+     */
+    input : ISignal<any>,
+
+    /** used to read multiple signals and to combine them to a single
+     * signal which dispatches an update when any input updates
+     */
+    inputs : ISignal<any>[],
+
+    /** delays signal processing by the given number of milliseconds,
+     * after delay time has passed, the latest value is processed,
+     * useful for throttling computation
+     */
+    delay: number,
+
+    /** if true then event handlers are awaited before retriggered,
+     * if value changes while the handler does not finish, the event
+     * handler is retriggered with the latest value, useful for throttling
+     * I/O operations, or other high cost operations
+     */
+    awaitListeners: boolean,
+
+    /** called when first the signal gets it's first ever subscribe
+     * useful for delaying oprations until sombody actually subscribes 
+     * to the object
+     */
+    onFirstSubscribe: () => void,
+
+    /** called every times somebody installs an event handler */
+    onSubscribe: () => void,
 }
 
 /** 
@@ -36,8 +75,13 @@ export interface ISignal<T>
     update(newValue: T) : void;
 
     /** adds a new change event handler to this signal */
-    subscribe: (handler: (value : T) => void) => () => void;
+    subscribe: (handler: (value : T) => void) => ISignalSubscription<T>;
 
     /** adds a new change event handler to the signal */
-    event: (handler: (value : T) => void) => () => void;
+    event: (handler: (value : T) => void) => ISignalSubscription<T>;
+}
+
+export interface ISignalSubscription<T>
+{
+    cancel : () => void;
 }
---
+++ test/signal.test.js
@@ -0,0 +1,57 @@
+const { createSignal } = require('..');
+
+test("has 'value' property which is r/w", () => {
+    const signal = createSignal();
+    expect(signal.value).not.toBe(42);
+    signal.value = 42;
+    expect(signal.value).toBe(42);
+});
+
+test("when property is set, event is triggered", () => {
+    const signal = createSignal();
+    let triggerCount = 0;
+    signal.event(() => triggerCount++);
+    expect(triggerCount).toBe(0);
+    signal.value = 42;
+    expect(triggerCount).toBe(1);
+});
+
+test("event handler can be removed", () => {
+    const signal = createSignal();
+    let triggerCount = 0;
+    const subscription = signal.event(() => triggerCount++);
+    expect(triggerCount).toBe(0);
+    signal.value = 42;
+    expect(triggerCount).toBe(1);
+    subscription.cancel();
+    signal.value = 13;
+    expect(triggerCount).toBe(1);
+});
+
+test("signal only notifies of changes", () => {
+    const signal = createSignal();
+    let triggerCount = 0;
+    signal.value = 13;
+    signal.event(() => triggerCount++);
+    signal.value = 13;
+    expect(triggerCount).toBe(0);
+    signal.value = 42;
+    expect(triggerCount).toBe(1);
+    signal.value = 42;
+    expect(triggerCount).toBe(1);
+})
+
+test("signal can recevive object comparer", () => {
+    const signal = createSignal({x:1,y:2}, {
+        equals: (a,b) => a.x === b.x && a.y === b.y
+    });
+    let triggerCount = 0;
+    signal.event(() => triggerCount++);
+    expect(triggerCount).toBe(0);
+    signal.value = {x:1, y:2};
+    expect(triggerCount).toBe(0);
+    signal.value = {x:1, y:3};
+    expect(triggerCount).toBe(1);
+    signal.value = {x:1, y:3};
+    expect(triggerCount).toBe(1);
+});
